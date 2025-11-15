c6253e23417d32a14167ac81c4d377e758525df4
neaL367 <atichatfantasy@gmail.com>
2025-11-15 13:41:02 +0700

    fix: prisma build failed

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,10 @@
-import { PrismaClient } from '@/app/generated/prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
+const prisma = new PrismaClient().$extends(withAccelerate());
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma
+export default prisma;
